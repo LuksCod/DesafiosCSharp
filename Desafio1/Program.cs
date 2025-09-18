071ddd769890f48185b328c8824a95b8f94d0f5b
LuksCod <Lucas.MTheodosio@gmail.com>
2025-09-18 02:05:56 -0300

    Desafios Finalizados

---
+++ Desafio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int soma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            soma += i;
+        }
+
+        Console.WriteLine($"A soma dos números de 1 a 10 é: {soma}");
+        Console.ReadKey();
+    }
+}
---
+++ Desafio3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Escolha a operação (+, -, *, /): ");
+        var operacao = Console.ReadLine();
+
+        double resultado = 0;
+
+        switch (operacao)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                    Console.WriteLine("Divisão por zero não é válido.");
+                break;
+            default:
+                Console.WriteLine("Calculo inválido!");
+                break;
+        }
+
+        Console.WriteLine($"Resultado: {resultado}");
+        Console.ReadKey();
+    }
+}
